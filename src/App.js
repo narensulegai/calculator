13b0e7ef3e763163e2b6d598673441a9cfb7f9c5
Naren Sulegai <86naren@gmail.com>
2022-11-11 22:18:42 -0800

    Adding full stop

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
                 <ul className="notes center mt-small">
                     <li>The display box is editable, you can directly type in the expression.</li>
                     <li>Valid expression are shown below the display.</li>
-                    <li>'=' button is disabled when expression is invalid</li>
+                    <li>'=' button is disabled when expression is invalid.</li>
                 </ul>
                 <div className="mt-small">
                     <Calculator/>
